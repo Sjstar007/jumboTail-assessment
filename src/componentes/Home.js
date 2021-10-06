663b162307601e89461e58e38de126f99c7b1e84
shubham jangid <shubhamjangid1699@gmail.com>
2021-10-06 09:25:07 +0530

    add mockSymbols with search functionality

--- src/componentes/Home.js
+++ src/componentes/Home.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Col, Form } from 'react-bootstrap';
+import { stockSymbolMockData } from '../mockSymbolData';
 import './home.css';
 
 export default function Home() {
+  const [mockStockData, setMockStockData] = useState(stockSymbolMockData);
   const [stockData, setStockData] = useState();
-  const [filteredData, setFilteredData] = useState();
-  const [trigerTable, setTrigerTable] = useState(null);
+  const [filteredData, setFilteredData] = useState(0);
+  const [trigerTable, setTrigerTable] = useState(0);
   // const [debounceTimeout, setDebounceTimeout] = useState(0);
   const fetchStockData = async () => {
     try {
       var response = {};
       var errored = false;
+      let symbol = trigerTable;
       response = await (
         await fetch(
-          `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo`
+          `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=40XSVEGMM4AF2O88`
         )
       ).json();
     } catch (err) {
@@ -32,16 +35,27 @@ export default function Home() {
   const search = event => {
     var search = event.target.value;
     setFilteredData(
-      stockData.filter(stocks =>
-        stocks.type.toUpperCase().includes(search.toUpperCase())
+      mockStockData.filter(stocks =>
+        stocks.toUpperCase().includes(search.toUpperCase())
       )
     );
+    console.log(filteredData);
+  };
+  const handleStock = getStock => {
+    setTrigerTable(getStock);
   };
   const getStockTable = stock => {
-    return <></>;
+    return (
+      <>
+        <tr>
+          <td onClick={() => handleStock(stock)}>{stock}</td>
+        </tr>
+      </>
+    );
   };
   useEffect(() => {
     getStockData();
+    console.log(trigerTable);
   }, []);
 
   return (
@@ -49,16 +63,17 @@ export default function Home() {
       <Container>
         <Col lg={12}>
           <Form.Control type="text" placeholder="Search..." onChange={search} />
-          {filteredData.length ? (
+
+          {filteredData.length && (
             <table>
               <thead>
                 <tr>
-                  <th></th>
+                  <th>Title</th>
                 </tr>
               </thead>
               <tbody>{filteredData.map(stock => getStockTable(stock))}</tbody>
             </table>
-          ) : null}
+          )}
         </Col>
       </Container>
     </>
---
+++ src/mockSymbolData.js
@@ -0,0 +1,1 @@
+export const stockSymbolMockData = ['IBM', 'INFY', 'CAP'];
